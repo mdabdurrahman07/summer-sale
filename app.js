ac5fe92405e8ab02fdba7a12159d3393b6be259b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-08-20 12:19:10 +0600

    cart section fully functional

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-let price = 0;
+let price = 0.00;
+let priceNum = parseFloat(price.toFixed(2))
+
 document.getElementById('card1').addEventListener('click' , function(){
   const itemOne = getItemName('item1')
   SetOfItems(itemOne)
   const money = getItemValue('money1')
-  console.log(typeof(money))
-     price = parseInt(price) + parseInt(money)
+
+    priceNum = priceNum + money  
+
     const totalPrice = document.getElementById('total-price')
-    totalPrice.innerText = price 
+    totalPrice.innerText = priceNum; 
+    if(priceNum > 0){
+        document.getElementById('purchase-btn').disabled = false;   
+    }
+    else{
+        document.getElementById('purchase-btn').disabled = true; 
+    }
+    if(priceNum >= 200){
+        document.getElementById('apply-btn').disabled = false;
+        
+    }
+    else{
+        document.getElementById('apply-btn').disabled = true;
+    }
+  
+})
+document.getElementById('apply-btn').addEventListener('click' , function(){
+             if(getInputValue('coupon-id') !== 'SELL200'){
+                return alert('Invalid Coupon')
+             }
+             else{
+                   const discountPrice = priceNum * 20 / 100
+                   document.getElementById('Discount-price').innerText = parseFloat(discountPrice.toFixed(2));
+                   const subtotal = priceNum - discountPrice 
+                   document.getElementById('Total-price').innerText = parseFloat(subtotal.toFixed(2))
+             }
 })
 
 
@@ -21,11 +49,18 @@ function getItemName(name){
     const ElementValue = Element.innerText
     return ElementValue
 }
+// fetching the input value
+function getInputValue(input){
+    const Element = document.getElementById(input)
+    const ElementValue2 = Element.value
+    console.log(ElementValue2)
+    return ElementValue2
+} 
 // fetch the item name and set it in the itemsArea
 function SetOfItems(items){
     const setArea = document.getElementById('itemsArea')
     const p = document.createElement('p')
-    p.classList.add('my-4')
+    p.classList.add('my-2')
     const count = setArea.childElementCount
     p.innerHTML = `<span class="text-xl font-medium">${count + 1}.  ${items}</span> `
     setArea.appendChild(p);
@@ -33,10 +68,10 @@ function SetOfItems(items){
 // fetching the items tk Amount
 function getItemValue(money){
     const Element = document.getElementById(money)
-    const ElementValue = Element.innerText
-    const ElementValueNumber = parseFloat(ElementValue)
+    const ElementValue3 = Element.innerText
+    const ElementValueNumber = parseFloat(ElementValue3)
     const ElementValueNumberDecimalString = ElementValueNumber.toFixed(2)
-    const ElementValueNumberDecimalNumber = parseInt(ElementValueNumberDecimalString)
+    const ElementValueNumberDecimalNumber = parseFloat(ElementValueNumberDecimalString)
     return ElementValueNumberDecimalNumber;
    
 }
